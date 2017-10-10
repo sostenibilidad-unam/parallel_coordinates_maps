e110a3f56349b465c79eca79f870f9617c042553
Fidel Serrano <serranoycandela@gmail.com>
2017-10-10 12:09:35 -0500

    adjust graph size

--- static/styles.js
+++ static/styles.js
@@ -84,20 +84,3 @@ var polygon_style = function(feature, resolution){
 };
 
 
-var polygon_style2 = new ol.style.Style({
-	  fill: new ol.style.Fill({color: hexToRGB(colorscale(0.9),0.65)}),
-	  stroke: new ol.style.Stroke({color: hexToRGB(colorscale(0.9),1),width: 1}),
-	  text: new ol.style.Text({
-		  	font: '12px Calibri,sans-serif',
-		  	fill: new ol.style.Fill({color: 'rgba(250,163,1,1)'}),
-	        stroke: new ol.style.Stroke({
-	        		color: 'rgba(100,100,100,1)',
-	        		width: 3
-	        })
-	  })
-});
-
-var point_style2 =  new ol.style.Style({
-    image: new ol.style.Circle({radius: 6.0 + size,
-        stroke: new ol.style.Stroke({color: 'rgba(191, 77, 179,1)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(191, 77, 179,0.65)'})})
-});
